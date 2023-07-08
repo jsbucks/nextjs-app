218829dab4393ff3c0d671620446f7d28d5129a1
jsbucks <mjweb2267@gmail.com>
2023-07-08 15:54:50 -0400

    fully running project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,28 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Provider from '@components/Provider'
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
-export const metagata = {
-    title: "Promtopia",
-    description: "Discover & Share AI Prompts"
-}
-const RootLayout = ({children}) => {
-    return (
-        <html lang="en">
-            <body>
-                <Provider>
-                <div className='main'>
-                    <div className='gradient'></div>
-                </div>
-                <main className='app'>
-                    <Nav />
-                    {children}
-                </main>
-                </Provider>
-            </body>
-        </html>
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-    )
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
